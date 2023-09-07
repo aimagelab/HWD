4c6688aab05b982b79f4baf3778265ee29c3a87e
Vittorio Pippi <219424@studenti.unimore.it>
2023-09-07 10:17:48 +0200

    Formatting scores

--- metrics/scores.py
+++ metrics/scores.py
@@ -1,11 +1,14 @@
+from datasets.transforms import ResizeHeight, ToTensor, PaddingMin, Compose, CropStartSquare, ResizeSquare
 from .base_score import BaseScore
 from .distances import EuclideanDistance, FrechetDistance, MaximumMeanDiscrepancy
 from .backbones import VGG16Backbone, InceptionV3Backbone
-from datasets.transforms import ResizeHeight, ToTensor, PaddingMin, Compose, CropStartSquare, ResizeSquare
+
+VGG16_10400_URL = 'https://github.com/aimagelab/font_square/releases/download/VGG-16/VGG16_class_10400.pth'
+INCEPTION_URL = 'https://github.com/mseitzer/pytorch-fid/releases/download/fid_weights/pt_inception-2015-12-05-6726825d.pth'
 
 class HWDScore(BaseScore):
     def __init__(self, device='cpu'):
-        backbone = VGG16Backbone('https://github.com/aimagelab/font_square/releases/download/VGG-16/VGG16_class_10400.pth', device=device)
+        backbone = VGG16Backbone(VGG16_10400_URL, device=device)
         distance = EuclideanDistance()
         transforms = Compose([
             ResizeHeight(32),
@@ -14,11 +17,10 @@ class HWDScore(BaseScore):
         ])
         super().__init__(backbone, distance, transforms)
 
+
 class FIDScore(BaseScore):
     def __init__(self, raise_on_error=False, batch_size=128, device='cpu'):
-        backbone = InceptionV3Backbone('https://github.com/mseitzer/pytorch-fid/releases/download/fid_weights/pt_inception-2015-12-05-6726825d.pth',
-                                       batch_size=batch_size,
-                                       device=device)
+        backbone = InceptionV3Backbone(INCEPTION_URL, batch_size=batch_size, device=device)
         distance = FrechetDistance(raise_on_error=raise_on_error)
         transforms = Compose([
             CropStartSquare(),
@@ -26,12 +28,11 @@ class FIDScore(BaseScore):
             ToTensor()
         ])
         super().__init__(backbone, distance, transforms)
-        
+
+
 class KIDScore(BaseScore):
     def __init__(self, batch_size=128, device='cpu', **kwargs):
-        backbone = InceptionV3Backbone('https://github.com/mseitzer/pytorch-fid/releases/download/fid_weights/pt_inception-2015-12-05-6726825d.pth',
-                                       batch_size=batch_size,
-                                       device=device)
+        backbone = InceptionV3Backbone(INCEPTION_URL, batch_size=batch_size, device=device)
         distance = MaximumMeanDiscrepancy(**kwargs)
         transforms = Compose([
             CropStartSquare(),
