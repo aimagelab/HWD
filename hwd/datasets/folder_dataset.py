8378b1521fdd95d9e8048fbcf6b3f95a2627ab5a
vittoriopippi <219424@studenti.unimore.it>
2024-11-05 08:33:47 +0100

    Folder dataset now accepts all images extensions by default

--- hwd/datasets/folder_dataset.py
+++ hwd/datasets/folder_dataset.py
@@ -3,9 +3,18 @@ from pathlib import Path
 
 
 class FolderDataset(BaseDataset):
-    def __init__(self, path, extension='png', **kwargs):
-        imgs = list(Path(path).rglob(f'*.{extension}'))
+    def __init__(self, path, extension=None, **kwargs):
+        imgs_ext = {
+            ".bmp", ".dib", ".dcx", ".eps", ".ps", ".gif",
+            ".icns", ".ico", ".im", ".jpeg", ".jpg", ".j2k",
+            ".j2p", ".jpx", ".msp", ".pcx", ".png", ".pbm",
+            ".pgm", ".ppm", ".pnm", ".sgi", ".spider",
+            ".tiff", ".tif", ".webp",".xbm"
+        } if extension is None else {extension}
+
+        imgs = [img for img in Path(path).rglob('*') if img.is_file() and img.suffix in imgs_ext]
         assert len(imgs) > 0, 'No images found.'
+        
         authors = [img.parent.name for img in imgs]
         author_ids = sorted(set(authors))
         super(FolderDataset, self).__init__(path, imgs, authors, author_ids, **kwargs)
